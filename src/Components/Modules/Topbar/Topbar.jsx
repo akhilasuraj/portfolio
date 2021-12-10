db2b488a03bc17c119a21dbcc6d924642ff9caa8
akhilasuraj <akhila.abesinghe@gmail.com>
2021-12-10 21:09:19 +0530

    hamburger menu animation created

--- src/Components/Modules/Topbar/Topbar.jsx
+++ src/Components/Modules/Topbar/Topbar.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import { useState } from "react";
 
 const Topbar = () => {
+  const [sideMenu, setSideMenu] = useState(false);
   return (
-    <div className="w-full h-32 bg-gray-300 flex items-center justify-between px-10 fixed top-0">
+    <div className="w-full h-28 bg-gray-300 flex items-center justify-between px-10">
       <div className="flex items-center">
         <span className="text-2xl font-monument cursor-pointer">AKHILA</span>
         <span className="ml-28 font-semibold cursor-pointer">
           CALL ME. (+94) 76 - 6857 611
         </span>
-        <span className="text-gray-400 font-normal text-xl px-10 cursor-default">/</span>
-        <span className="font-semibold cursor-pointer">akhila.abesinghe@gmail.com</span>
+        <span className="text-gray-400 font-normal text-xl px-10 cursor-default">
+          /
+        </span>
+        <span className="font-semibold cursor-pointer">
+          akhila.abesinghe@gmail.com
+        </span>
       </div>
-      <div className="w-9 h-7 flex flex-col justify-between cursor-pointer">
-        <span className="w-full h-1 bg-black"></span>
-        <span className="w-full h-1 bg-black"></span>
-        <span className="w-full h-1 bg-black"></span>
+      <div
+        className="w-9 h-7 flex flex-col justify-between cursor-pointer"
+        onClick={() => setSideMenu((n) => !n)}
+      >
+        <span
+          className={`w-full h-1 bg-black origin-left transform ease-in-out duration-700 ${
+            sideMenu ? "rotate-45" : ""
+          }`}
+        />
+        <span
+          className={`w-full h-1 bg-black transform ease-in-out duration-700 ${
+            sideMenu ? "opacity-0 scale-x-0" : ""
+          }`}
+        />
+        <span
+          className={`w-full h-1 bg-black origin-left transform ease-in-out duration-700 ${
+            sideMenu ? "-rotate-45" : ""
+          }`}
+        />
       </div>
     </div>
   );
